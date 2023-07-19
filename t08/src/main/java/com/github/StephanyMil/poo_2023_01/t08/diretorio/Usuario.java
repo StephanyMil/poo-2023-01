a7d20161824cea536335597f83c2434c355a41ef
Stephany Milhomem <110176560+StephanyMil@users.noreply.github.com>
2023-07-19 20:06:45 -0300

    Update Usuario.java

--- t08/src/main/java/com/github/StephanyMil/poo_2023_01/t08/diretorio/Usuario.java
+++ t08/src/main/java/com/github/StephanyMil/poo_2023_01/t08/diretorio/Usuario.java
@@ -2,6 +2,7 @@ package com.github.StephanyMil.poo_2023_01.t08.diretorio;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class Usuario {
     private String nome;
     private List<Diretorio> diretorios;
