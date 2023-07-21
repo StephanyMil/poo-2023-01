6246ce586d68abfbe1a0cae537897254fd4c8823
Stephany Milhomem <110176560+StephanyMil@users.noreply.github.com>
2023-07-21 17:22:01 -0300

    Update PessoaComAutoRelacionamento.java

--- t07/src/main/java/com/github/StephanyMil/poo_2023_01/t07/pessoa/PessoaComAutoRelacionamento.java
+++ t07/src/main/java/com/github/StephanyMil/poo_2023_01/t07/pessoa/PessoaComAutoRelacionamento.java
@@ -1,6 +1,7 @@
 package com.github.StephanyMil.poo_2023_01.t07.pessoa;
 
 import java.util.Date;
+
 public class PessoaComAutoRelacionamento {
     private Date nascimento;
     private String nome;
