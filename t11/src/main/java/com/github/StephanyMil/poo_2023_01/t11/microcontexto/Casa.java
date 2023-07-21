4adef2db871e924e2117e9be890faebb3ea42750
Stephany Milhomem <110176560+StephanyMil@users.noreply.github.com>
2023-07-21 20:36:23 -0300

    Update Casa.java

--- t11/src/main/java/com/github/StephanyMil/poo_2023_01/t11/microcontexto/Casa.java
+++ t11/src/main/java/com/github/StephanyMil/poo_2023_01/t11/microcontexto/Casa.java
@@ -2,6 +2,7 @@ package com.github.StephanyMil.poo_2023_01.t11.microcontexto;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class Casa {
     private List<Quarto> quartos;
     private List<Banheiro> banheiros;
