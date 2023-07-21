6d5d6a6cd7705d46bb65a8ae5d69e52f9f5a9373
Stephany Milhomem <110176560+StephanyMil@users.noreply.github.com>
2023-07-21 20:41:06 -0300

    Update Prova.java

--- t17/src/main/java/com/github/StephanyMil/poo_2023_01/t17/avaliacao/Prova.java
+++ t17/src/main/java/com/github/StephanyMil/poo_2023_01/t17/avaliacao/Prova.java
@@ -2,6 +2,7 @@ package com.github.StephanyMil.poo_2023_01.t17.avaliacao;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class Prova {
     private List<Aluno> alunos;
     private Avaliacao avaliacao;
